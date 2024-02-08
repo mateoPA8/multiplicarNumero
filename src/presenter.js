8168d1e137b33af65e84147970c7e1be826fe45e
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-02-08 10:45:55 -0400

    se anadio la funcionalidad de multiplicar 2 numeros

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,30 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
+const form = document.querySelector("#sumar-form");
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const divSumar = document.querySelector("#resultadoSumar-div");
 
+const first1 = document.querySelector("#primer1-numero");
+const second2 = document.querySelector("#segundo2-numero");
+const form2 = document.querySelector("#multiplicar-form");
+const divMultiplicar = document.querySelector("#resultadoMultiplicar-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  divSumar.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+ 
+});
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber1 = Number.parseInt(first1.value);
+  const secondNumber2 = Number.parseInt(second2.value);
+
+ 
+  divMultiplicar.innerHTML = "<p>" + multiplicar(firstNumber1, secondNumber2) + "</p>";
 });
